11219a8aa58c23cb3cc9fd8edec30fa79bcd8270
steven.luo <steven.luo>
2017-01-09 17:29:31 -0500

    First Test Push

---
+++ WC-Script/src/main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.dreambot.api.script.AbstractScript;
+
+/**
+ * Created by steven.luo on 09/01/2017.
+ */
+public class main extends AbstractScript {
+    @Override
+    public int onLoop() {
+        return 0;
+    }
+}
