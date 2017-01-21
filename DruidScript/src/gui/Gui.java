443dabb087ffc86714ff7435ebf523b48c1dee6f
stevenl4 <steven.y.luo@gmail.com>
2017-01-21 15:45:10 -0500

    finish druid script gui

---
+++ DruidScript/src/gui/Gui.java
@@ -0,0 +1,84 @@
+package gui;
+
+import util.ScriptVars;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Steven on 1/21/2017.
+ */
+public class Gui extends JFrame {
+    private JPanel contentPane;
+    private JTextField txtFoodName;
+    private JTextField txtFoodAmt;
+    JCheckBox chckbxHopWorlds = new JCheckBox("hop worlds");
+
+    public Gui(final ScriptVars var){
+        setTitle("DreamBot");
+        setAlwaysOnTop(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 400, 500);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.WHITE);
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 400, 412);
+        contentPane.add(panel);
+        panel.setLayout(null);
+
+        // Food Name
+        JLabel lblFoodName = new JLabel("Food Name:");
+        lblFoodName.setBounds(0, 10, 90, 15);
+        panel.add(lblFoodName);
+
+        txtFoodName = new JTextField();
+        txtFoodName.setText("Tuna");
+        txtFoodName.setBounds(100, 10, 150, 20);
+        panel.add(txtFoodName);
+        txtFoodName.setColumns(10);
+
+        // Food Amount
+        JLabel lblFoodAmt = new JLabel("Food Amount:");
+        lblFoodAmt.setBounds(0, 35, 90, 15);
+        panel.add(lblFoodAmt);
+
+        txtFoodAmt = new JTextField();
+        txtFoodAmt.setText("0");
+        txtFoodAmt.setBounds(100, 35, 150, 20);
+        panel.add(txtFoodAmt);
+        txtFoodAmt.setColumns(10);
+
+
+        JLabel lblHopWorlds = new JLabel("Hop Worlds:");
+        lblHopWorlds.setBounds(0, 60, 90, 15);
+        panel.add(lblHopWorlds);
+
+        chckbxHopWorlds.setBounds(100, 60, 100, 20);
+        chckbxHopWorlds.setSelected(true);
+        panel.add(chckbxHopWorlds);
+
+        JButton btnNewButton = new JButton("Start!");
+        btnNewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                var.foodName = txtFoodName.getText();
+                var.requiredFoodAmt = Integer.parseInt(txtFoodAmt.getText());
+                var.hop = chckbxHopWorlds.isSelected();
+                var.started = true;
+                dispose();
+            }
+        });
+
+        btnNewButton.setBounds(0,421,400,50);
+        contentPane.add(btnNewButton);
+    }
+}
--- DruidScript/src/util/ScriptVars.java
+++ DruidScript/src/util/ScriptVars.java
@@ -10,8 +10,10 @@ public class ScriptVars {
     public String[] universalLoot = new String[]{"Loop half of key", "Tooth half of key", "Nature rune", "Runite bar", "Rune spear", "Rune battleaxe", "Rune 2h sword", "Uncut diamond", "Rune sq shield", "Rune arrow", "Law rune", "Death rune", "Dragonstone", "Rune kiteshield", "Dragon med helm", "Shield left half", "Dragon spear"};
     public String[] uniqueLoot;
     public String[] loot;
-    public boolean useShortcut = false;
     public boolean started = false;
-
+    public boolean hop;
+    public boolean testMode = false;
+    public String foodName;
+    public int requiredFoodAmt;
 
 }
